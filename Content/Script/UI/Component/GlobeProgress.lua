c9ad4962699919c4268057198a69398d1dad762e
ppgood <928826801@qq.com>
2025-08-15 19:25:32 +0800

    feat:增加血条残影

--- Content/Script/UI/Component/GlobeProgress.lua
+++ Content/Script/UI/Component/GlobeProgress.lua
@@ -16,6 +16,7 @@ function M:PreConstruct(IsDesignTime)
     self.Image_Background:SetBrush(self.BackgroundBrush)
     self.Image_Glass:SetBrush(self.GlassBrush)
     self.ProgressBar_Globe.WidgetStyle = self.ProgressBarStyle
+    self.ProgressBar_Ghost.WidgetStyle.FillImage = self.GhostFillBrush
 end
 
 return M
