f3a90a9370bcb70a391865664d1249e8d623b0c6
ppgood <928826801@qq.com>
2025-08-15 19:31:39 +0800

    fix:去除蓝图逻辑

--- Content/Script/UI/Component/GlobeProgress.lua
+++ Content/Script/UI/Component/GlobeProgress.lua
@@ -15,7 +15,7 @@ local M = UnLua.Class()
 function M:PreConstruct(IsDesignTime)
     self.Image_Background:SetBrush(self.BackgroundBrush)
     self.Image_Glass:SetBrush(self.GlassBrush)
-    self.ProgressBar_Globe.WidgetStyle = self.ProgressBarStyle
+    self.ProgressBar_Globe.WidgetStyle.FillImage = self.GlobeFillBrush
     self.ProgressBar_Ghost.WidgetStyle.FillImage = self.GhostFillBrush
 end
 
