ac90e13c87eecb4fe525ce9c2ea1e1b9fe6787d7
ppgood <928826801@qq.com>
2025-08-21 18:17:49 +0800

    feat:GA发射火球播放蒙太奇动画

---
+++ Content/Script/GA/GA_FireBolt.lua
@@ -0,0 +1,42 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+
+---@type GA_FireBolt_C
+local M = UnLua.Class()
+function M:K2_ActivateAbility()
+    UE.UKismetSystemLibrary.PrintString(WorldContectObject,"准备播放动画ActivateTest")
+    local Proxy = UE.UAbilityTask_PlayMontageAndWait.CreatePlayMontageAndWaitProxy(
+        self, nil, self.FireAnim, 1, nil, true, 1.0, 0)
+    Proxy:ReadyForActivation()
+
+    -- local P = UE.UAbilityTask_WaitGameplayEvent.WaitGameplayEvent(self,self.WaitTag,nil,false,true)
+
+    UE.UKismetSystemLibrary.PrintString(WorldContectObject,"测试GA开始ActivateTest")
+end
+-- eg
+-- function M:K2_ActivateAbility()
+--     if self:IsLocallyControlled() then
+--         self:StartRangedWeaponTargeting()
+--     end
+
+--     local Proxy = UE.UAbilityTask_PlayMontageAndWait.CreatePlayMontageAndWaitProxy(self, nil, self.CharacterFireMontage, self.AutoRate, nil, false, 1.0, 0)
+--     local Handler = function()
+--         self:K2_EndAbility()
+--     end
+--     Proxy.OnCompleted:Add(self, Handler)
+--     Proxy.OnInterrupted:Add(self, Handler)
+--     Proxy.OnCancelled:Add(self, Handler)
+--     Proxy:ReadyForActivation()
+
+--     UE.UKismetSystemLibrary.K2_SetTimerDelegate({ self, self.FireComplete }, self.FireDelayTimeSecs, false, 0, 0)
+-- end
+
+-- function M:FireComplete()
+--     self:K2_EndAbility()
+-- end
+return M
--- Content/Script/GA/GA_Test.lua
+++ Content/Script/GA/GA_Test.lua
@@ -11,20 +11,20 @@ local M = UnLua.Class()
 
 function M:K2_ActivateAbility()
     print("[PeiLuaLog]测试GA开始")
-    UE4.UKismetSystemLibrary.PrintString(WorldContectObject,"测试GA开始ActivateTest")
+    UE.UKismetSystemLibrary.PrintString(WorldContectObject,"测试GA开始ActivateTest")
     self:DelayF(2);
 
 end
 
 function M:K2_OnEndAbility(bWasCancelled)
     print("[PeiLuaLog]测试GA结束")
-    UE4.UKismetSystemLibrary.PrintString(WorldContectObject,"测试GA结束EndTest")
+    UE.UKismetSystemLibrary.PrintString(WorldContectObject,"测试GA结束EndTest")
 end
 
 function M:DelayF(Induration)
     coroutine.resume(coroutine.create(
         function(WorldContectObject,duration)    
-            UE4.UKismetSystemLibrary.Delay(WorldContectObject,duration)
+            UE.UKismetSystemLibrary.Delay(WorldContectObject,duration)
             self:K2_EndAbility()
         end
     ),
