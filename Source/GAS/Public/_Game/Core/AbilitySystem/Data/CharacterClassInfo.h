ab8c431d620b344481b49a2d07184f835f3935bd
ppgood <928826801@qq.com>
2025-08-25 13:41:31 +0800

    feat:初始化敌人属性

---
+++ Source/GAS/Public/_Game/Core/AbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+class UGameplayEffect;
+
+//角色职业类型的枚举
+UENUM(BlueprintType)
+enum class ECharacterClass : uint8
+{
+	Elementalist, //法师
+	Warrior, //战士
+	Ranger //游侠
+};
+
+USTRUCT(BlueprintType)
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly,Category="Class Defaults")
+	TSubclassOf<UGameplayEffect> PrimaryAttributes;
+};
+/**
+ * 
+ */
+UCLASS()
+class GAS_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+	//通过职业去更换不同的主要属性从而影响其他的属性
+	UPROPERTY(EditDefaultsOnly, Category="Class Defaults")
+	TMap<ECharacterClass, FCharacterClassDefaultInfo> CharacterClassInformation;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> SecondaryAttributes;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Common Class Defaults")
+	TSubclassOf<UGameplayEffect> VitalAttributes;
+
+	//通过枚举获取对应的初始化类
+	FCharacterClassDefaultInfo GetClassDefaultInfo(ECharacterClass CharacterClass);
+
+};
